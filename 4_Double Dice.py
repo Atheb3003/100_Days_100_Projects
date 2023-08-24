a711253b638ffac097980a999ff69d384eac3397
Antonio Mérida <131478083+Atheb3003@users.noreply.github.com>
2023-08-24 11:24:00 +0200

    Add files via upload

--- 4_Double Dice.py
+++ 4_Double Dice.py
@@ -43,142 +43,54 @@ six = """
 │  ●   ●  │
 └─────────┘
 """
-
 dices = int(input("How many dices do you want to use? (You can only use 1 or 2): "))
+first_dice = None
+second_dice = None
 
-opt = None
-
-if dices == 1:
+if dices >= 1:
     random_dice = random.randrange(1,6)
     if random_dice == 1:
         print(one)
-        print("Your number is 1")
+        first_dice = 1
     elif random_dice ==2:
         print(two)
-        print("Your number is 2")
+        first_dice = 2
     elif random_dice ==3:
         print(three)
-        print("Your number is 3")
+        first_dice = 3
     elif random_dice ==4:
         print(four)
-        print("Your number is 4")
+        first_dice = 4
     elif random_dice ==5:
         print(five)
-        print("Your number is 5")
-    else:
-        print(six)
-        print("Your number is 6")
-
-elif dices == 2:
-    random_dice = random.randrange(2, 12)
-    if random_dice == 2:
-        print(one, one)
-        print("Your number is 2")
-
-    elif random_dice == 3:
-        opt = random.randrange(1,4)
-        if opt == 1:
-            print(one, two)
-        else:
-            print(two, one)
-        print("Your number is 3")
-
-    elif random_dice == 4:
-        opt = random.randrange(1,3)
-        if opt == 1:
-            print(one, three)
-        elif opt == 2:
-            print(two, two)
-        else:
-            print(three, one)
-        print("Your number is 4")
-
-    elif random_dice == 5:
-        opt = random.randrange(1,4)
-        if opt == 1:
-            print(one, four)
-        elif opt == 2:
-            print(two, three)
-        elif opt == 3:
-            print(three, two)
-        else:
-            print(four, one)
-        print("Your number is 5")
-
+        first_dice = 5
     elif random_dice == 6:
-        opt = random.randrange(1,5)
-        if opt == 1:
-            print(one, five)
-        elif opt == 2:
-            print(two, four)
-        elif opt == 3:
-            print(three, three)
-        elif opt == 4:
-            print(four, two)
-        else:
-            print(five, one)
-        print("Your number is 6")
+        print(six)
+        first_dice = 6
+    if dices == 1:
+        result = first_dice
+        print("The result is {}".format(result))
 
-    elif random_dice == 7:
-        opt = random.randrange(1,6)
-        if opt == 1:
-            print(one, six)
-        elif opt == 2:
-            print(two, five)
-        elif opt == 3:
-            print(three, four)
-        elif opt == 4:
-            print(four, three)
-        elif opt == 5:
-            print(five, two)
-        else:
-            print(six, one)
-        print("Your number is 7")
+if dices == 2:
+    random_dice_2 = random.randrange(1,6)
+    if random_dice_2 == 1:
+        print(one)
+        second_dice = 1
+    elif random_dice_2 ==2:
+        print(two)
+        second_dice = 2
+    elif random_dice_2 ==3:
+        print(three)
+        second_dice = 3
+    elif random_dice_2 ==4:
+        print(four)
+        second_dice = 4
+    elif random_dice_2 ==5:
+        print(five)
+        second_dice = 5
+    elif random_dice_2 == 6:
+        print(six)
+        second_dice = 6
+    result = first_dice + second_dice
+    print("The result is {}".format(result))
 
-    elif random_dice == 8:
-        opt = random.randrange(1,5)
-        if opt == 1:
-           print(two, six)
-        elif opt == 2:
-            print(three, five)
-        elif opt == 3:
-            print(four, four)
-        elif opt == 4:
-            print(five, three)
-        else:
-            print(six, two)
-        print("Your number is 8")
-
-    elif random_dice == 9:
-        opt = random.randrange(1,4)
-        if opt == 1:
-            print(three, six)
-        elif opt == 2:
-            print(four, five)
-        elif opt == 3:
-            print(five, four)
-        else:
-            print(six, three)
-        print("Your number is 9")
-
-    elif random_dice == 10:
-        opt = random.randrange(1,3)
-        if opt == 1:
-            print(four, six)
-        elif opt == 2:
-            print(five, five)
-        else:
-            print(six, four)
-        print("Your number is 10")
-    
-    elif random_dice == 11:
-        opt = random.randrange(1,2)
-        if opt == 1:
-            print(five, six)
-        else:
-            print(six, five)
-        print("Your number is 11")
-    
-    else:
-        print(six, six)
-        print("Your number is 12")
